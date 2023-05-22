77fdcd64701506b5f30fec3916a37fd3752ca04a
bhadresh gosai <45452903+bhadresh-goswami@users.noreply.github.com>
2023-05-22 21:27:13 +0530

    Add project files.

---
+++ Demo-Brijesh-Patiil/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Demo_Brijesh_Patiil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
